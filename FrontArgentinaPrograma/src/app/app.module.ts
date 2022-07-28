65a3e0be048fa2aa72de9a204adc5997b859b186
xMercurialx <100611284+xMercurialx@users.noreply.github.com>
2022-07-28 17:11:38 -0300

    componentes

---
+++ FrontArgentinaPrograma/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BanerComponent } from './componentes/baner/baner.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HysComponent } from './componentes/hys/hys.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { SocialComponent } from './componentes/social/social.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BanerComponent,
+    EducationComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    HysComponent,
+    InicioComponent,
+    LoginComponent,
+    LogoComponent,
+    ProyectosComponent,
+    SocialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
